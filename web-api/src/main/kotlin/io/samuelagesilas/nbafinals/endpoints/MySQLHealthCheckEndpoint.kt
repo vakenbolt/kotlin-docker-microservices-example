d43927033a1ab2388ec3bdfddacb30067d909d7e
Samuel A <desk.samuel.a@gmail.com>
2019-04-10 17:58:41 -0500

    Added Redis health check

--- web-api/src/main/kotlin/io/samuelagesilas/nbafinals/endpoints/MySQLHealthCheckEndpoint.kt
+++ web-api/src/main/kotlin/io/samuelagesilas/nbafinals/endpoints/MySQLHealthCheckEndpoint.kt
@@ -2,33 +2,52 @@ package io.samuelagesilas.nbafinals.endpoints
 
 import io.samuelagesilas.nbafinals.core.Paths
 import io.samuelagesilas.nbafinals.modules.Endpoint
-import io.vertx.core.http.HttpServerResponse
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.RoutingContext
+import redis.clients.jedis.Jedis
 import javax.inject.Inject
 import javax.sql.DataSource
 
 
-class MySQLHealthCheckEndpoint @Inject constructor(router: Router, dataSource: DataSource) : Endpoint {
+class MySQLHealthCheckEndpoint @Inject constructor(router: Router,
+                                                   mySQLHealthCheckResolver: MySQLHealthCheckResolver) : Endpoint {
 
     private val mysqlRoute = router.route(Paths.healthCheck)
 
     init {
-        mysqlRoute.blockingHandler { ctx -> healthCheck(ctx, dataSource) }
+        mysqlRoute.blockingHandler { ctx ->
+            val response = ctx.response()
+            arrayOf(mySQLHealthCheckResolver.mySqlHealthCheck(),
+                    mySQLHealthCheckResolver.redisHealthCheck())
+                    .let { results ->
+                        when (results.contains(false)) {
+                            true -> response.setStatusCode(500).end()
+                            false -> response.setStatusCode(200).end()
+                        }
+                    }
+        }
     }
 }
 
 
-fun healthCheck(routingContext: RoutingContext, dataSource: DataSource) {
-    val response: HttpServerResponse = routingContext.response();
-    var isHealthy = true
-    try {
-        dataSource.connection.use { it.createStatement().use { statement -> statement.execute("SELECT 1") } }
-    } catch (e: Exception) {
-        isHealthy = false
+class MySQLHealthCheckResolver @Inject constructor(private val dataSource: DataSource,
+                                                   private val redis: Jedis) {
+
+    fun mySqlHealthCheck(): Boolean {
+        return try {
+            dataSource.connection.use { it.createStatement().use { statement -> statement.execute("SELECT 1") } }
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
-    if (isHealthy)
-        response.setStatusCode(200).end()
-    else
-        response.setStatusCode(500).end()
+
+    fun redisHealthCheck(): Boolean {
+        return try {
+            redis.use { it.ping().toLowerCase() == "pong" }
+        } catch (e: Exception) {
+            false
+        }
+    }
 }
+
+
