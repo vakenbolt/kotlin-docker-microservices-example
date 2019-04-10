8305c4a26c76b9ee92708b3e1be41265b94717d8
Samuel A <desk.samuel.a@gmail.com>
2019-04-10 18:40:40 -0500

    Small naming update around resolvers.

--- web-api/src/main/kotlin/io/samuelagesilas/nbafinals/endpoints/AuthenticationEndpoint.kt
+++ web-api/src/main/kotlin/io/samuelagesilas/nbafinals/endpoints/AuthenticationEndpoint.kt
@@ -16,4 +16,3 @@ class AuthenticationEndpoint @Inject constructor(respond: Resolver,
         }
     }
 }
-
--- web-api/src/main/kotlin/io/samuelagesilas/nbafinals/modules/EndpointsModule.kt
+++ web-api/src/main/kotlin/io/samuelagesilas/nbafinals/modules/EndpointsModule.kt
@@ -4,7 +4,7 @@ import com.google.inject.AbstractModule
 import com.google.inject.TypeLiteral
 import com.google.inject.multibindings.Multibinder
 import io.samuelagesilas.nbafinals.endpoints.AuthenticationEndpoint
-import io.samuelagesilas.nbafinals.endpoints.MySQLHealthCheckEndpoint
+import io.samuelagesilas.nbafinals.endpoints.HealthCheckEndpoint
 import io.samuelagesilas.nbafinals.endpoints.NBAChampionsEndpoint
 
 interface Endpoint {}
@@ -13,7 +13,7 @@ class EndpointsModule() : AbstractModule() {
 
     override fun configure() {
         val multiBinder = Multibinder.newSetBinder(binder(), object : TypeLiteral<Endpoint>() {})
-        multiBinder.addBinding().to(MySQLHealthCheckEndpoint::class.java)
+        multiBinder.addBinding().to(HealthCheckEndpoint::class.java)
         multiBinder.addBinding().to(NBAChampionsEndpoint::class.java)
         multiBinder.addBinding().to(AuthenticationEndpoint::class.java)
     }
