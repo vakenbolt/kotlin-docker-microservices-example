7e90497f65b04a25474bd6d45821e4d38758fd55
Samuel A <desk.samuel.a@gmail.com>
2019-04-09 23:00:06 -0500

    Fixed issuew with sql object plugin config and gradle

--- web-api/src/main/kotlin/io/samuelagesilas/nbafinals/modules/JdbiModule.kt
+++ web-api/src/main/kotlin/io/samuelagesilas/nbafinals/modules/JdbiModule.kt
@@ -3,7 +3,9 @@ package io.samuelagesilas.nbafinals.modules
 import com.google.inject.AbstractModule
 import com.google.inject.Provides
 import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.KotlinPlugin
 import org.jdbi.v3.sqlobject.SqlObjectPlugin
+import org.jdbi.v3.sqlobject.kotlin.KotlinSqlObjectPlugin
 import javax.sql.DataSource
 
 class JdbiModule: AbstractModule() {
@@ -11,7 +13,10 @@ class JdbiModule: AbstractModule() {
     @Provides
     fun providesJdbi(dataSource: DataSource): Jdbi {
         val jdbi = Jdbi.create(dataSource)
-        jdbi.installPlugins().installPlugin(SqlObjectPlugin())
+        jdbi.installPlugins()
+            .installPlugin(SqlObjectPlugin())
+            .installPlugin(KotlinPlugin())
+            .installPlugin(KotlinSqlObjectPlugin())
         return jdbi
     }
 }
