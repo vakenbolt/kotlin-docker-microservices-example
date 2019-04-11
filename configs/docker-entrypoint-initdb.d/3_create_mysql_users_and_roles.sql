083ce7f70caac3ee76132a82f1192a254ef3f654
Samuel A <desk.samuel.a@gmail.com>
2019-04-10 20:15:35 -0500

    Create Dockerfile for api server and updated docker-compose-integration.

--- configs/docker-entrypoint-initdb.d/3_create_mysql_users_and_roles.sql
+++ configs/docker-entrypoint-initdb.d/3_create_mysql_users_and_roles.sql
@@ -1,3 +1,5 @@
 CREATE USER 'nba_finals_service'@'localhost' IDENTIFIED BY 'ilovethisgame';
+CREATE USER 'nba_finals_service'@'nba_finals_web_api' IDENTIFIED BY 'ilovethisgame';
 GRANT SELECT ON nba_finals.champions TO 'nba_finals_service'@'localhost';
+GRANT SELECT ON nba_finals.champions TO 'nba_finals_service'@'nba_finals_web_api';
 FLUSH PRIVILEGES;
