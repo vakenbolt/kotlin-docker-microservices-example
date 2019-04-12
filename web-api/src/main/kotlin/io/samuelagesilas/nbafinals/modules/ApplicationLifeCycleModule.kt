810421d83c6aafdb5a9d3b34b9e37e5decca237f
Samuel A <vakenbolt@users.noreply.github.com>
2019-04-12 17:58:40 -0500

    Adds shutdownTimeoutSeconds property to json config files. (#12)

--- web-api/src/main/kotlin/io/samuelagesilas/nbafinals/modules/ApplicationLifeCycleModule.kt
+++ web-api/src/main/kotlin/io/samuelagesilas/nbafinals/modules/ApplicationLifeCycleModule.kt
@@ -1,11 +1,41 @@
 package io.samuelagesilas.nbafinals.modules
 
 import com.google.inject.AbstractModule
-import io.samuelagesilas.nbafinals.core.ApplicationLifeCycleModule
+import com.google.inject.Inject
+import com.zaxxer.hikari.HikariDataSource
+import io.samuelagesilas.nbafinals.core.NBAFinalsApiVerticle
+import io.vertx.core.Vertx
+import redis.clients.jedis.Jedis
 
 class ApplicationLifeCycleModule : AbstractModule() {
 
     override fun configure() {
         bind(ApplicationLifeCycleModule::class.java).asEagerSingleton()
+    }
+}
+
+
+class ApplicationLifeCycle @Inject constructor(private val vertx: Vertx,
+                                               private val verticle: NBAFinalsApiVerticle,
+                                               private val dataSource: HikariDataSource,
+                                               private val redis: Jedis,
+                                               private val serverConfig: ServerConfig) {
+
+    private val shutdownHook = object : Thread() {
+        override fun run() {
+            with(ApiServerShutdown(verticle, vertx), ::runThreadAndWait)
+            with(HikariShutdown(dataSource), ::runThreadAndWait)
+            with(RedisShutdown(redis), ::runThreadAndWait)
+        }
+
+        private fun runThreadAndWait(thread: Thread) {
+            thread.start()
+            thread.join(serverConfig.shutdownTimeoutSeconds)
+        }
+    }
+
+    fun start() {
+        Runtime.getRuntime().addShutdownHook(this.shutdownHook)
+        vertx.deployVerticle(verticle)
     }
 }
